403d8e466e567d997e6dc8ab0ccfb0da15e9d588
Nate Morrison <natem@nvsd.ca>
2022-02-05 00:54:36 -0800

    modified:   full-debloat.sh 	new file:   sysapps.ods

--- full-debloat.sh
+++ full-debloat.sh
@@ -1,19 +1,32 @@
 #!/bin/sh
 
+printf "This script has only been tested Android 11. There is no guarantee this will work on other versions of Android.\n";
+printf "WARNING: The following apps and their data will be removed.\nThis CANNOT BE UNDONE"
+printf "\n"
+
 # Connect to the device and remount directories as rw
 adb su
 su
-mount -ro remount,rw /
-mount -ro remount,rw /product
-mount -ro remount,rw /sys
-mount -ro remount,rw /system
-mount -ro remount,rw /vendor
-mount -ro remount,rw /data
+mount -rov remount,rw /
+mount -rov remount,rw /product
+mount -rov remount,rw /sys
+mount -rov remount,rw /system
+mount -rov remount,rw /vendor
+mount -rov remount,rw /data
+
+# Disable apps scheduled for removal, to prevent broken icons and uncleared caches
+pm uninstall --user com.
 
 # Debloat
-rm -Rf /product/preload
+rm -fv /bin/bootanimation	# Useless; just wastes time
+rm -Rfv /product/preload
 cd /system/app
 # Google
-rm -Rf YouTube BookmarkProvider Chrome ChromeCustomizations DuoStub EasterEgg Gmail2 GoogleCalendarSyncAdaptor Google ContactsSyncAdaptor GoogleExtShared GoogleLocationHistory GooglePrintRecommendationService GoogleTTS 
+rm -Rfv 
 # Samsung
-rm -Rf ClipboardEdge ClockPackage CompanionDeviceManager SamsungOne
+rm -Rfv 
+cd ../priv-app
+#Google
+rm -Rfv
+# Samsung
+rm -Rfv
